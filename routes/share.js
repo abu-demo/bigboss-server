0b1e99e582de503eb8a4d156d457ef15595c0d06
abu <abusuper@outlook.com>
2019-12-23 22:58:41 +0800

    share load more

--- routes/share.js
+++ routes/share.js
@@ -2,9 +2,26 @@ const router = require('koa-router')()
 const $sql = require('../utils/sql_map')
 const query = require('../utils/mysql')
 
+router.prefix('/share')
 
-router.get('/share', async (ctx, next) => {
+// 获取文章
+router.get('/', async (ctx, next) => {
   await query($sql.share.get).then(res => {
+    ctx.body = res
+  })
+})
+
+// 获取 更多 文章
+router.get('/more', async (ctx, next) => {
+  let params = ctx.request.query;
+  await query(`${$sql.share.more} ${params[0]} , ${params[1]};`).then(res => {
+    ctx.body = res
+  })
+})
+
+// 获取 文章 总数
+router.get('/sum', async (ctx, next) => {
+  await query($sql.share.sum_share).then(res => {
     ctx.body = res
   })
 })
--- utils/sql_map.js
+++ utils/sql_map.js
@@ -1,22 +1,24 @@
 let sql_map = {
     user: {
-        personal: 'SELECT  id, name, role, sign, logo, education, contact, web FROM `user` where id = ?;',
+        personal: 'SELECT  id, name, role, sign, logo, education, contact, web FROM `user` where id = ? limit 1;',
         register: 'INSERT INTO `user` VALUES(0, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
-        login: 'SELECT  id, name, password, role, sign, logo, education, contact, web FROM `user` where id = ?;'
+        login: 'SELECT  id, name, password, role, sign, logo, education, contact, web FROM `user` where id = ? limit 1;'
     },
     share: {
-        get: 'SELECT id, title, summary, author, author_id, time, discuss FROM `share` order by id desc;',
+        get: 'SELECT id, title, summary, author, author_id, time, discuss FROM `share` order by id desc limit 4 , 4;',
+        sum_share: 'SELECT count(*) FROM `share`;',
+        more: 'SELECT id, title, summary, author, author_id, time, discuss FROM `share` order by id desc limit ',
     },
     feedback: {
-        get: 'SELECT id, name, time, text FROM `feedback` order by id desc;',
+        get: 'SELECT id, name, time, text FROM `feedback` order by id desc limit 10;',
         insert_comment: 'INSERT INTO `feedback` VALUES(0, ?, ?, ?);',
     },
     detail: {
-        get_artical: 'SELECT * FROM `share` WHERE ID = ?;',
+        get_artical: 'SELECT * FROM `share` WHERE ID = ? limit 1;',
         get_comment: 'SELECT * FROM `share_comment` WHERE share_id = ? order by id desc;',
         insert_comment: 'INSERT INTO `share_comment` VALUES(0, ?, ?, ?, ?, ?);',
-        delete_comment: 'DELETE FROM `share_comment` WHERE id = ?;',
-        delete_article: 'DELETE FROM `share` WHERE id = ?;'
+        delete_comment: 'DELETE FROM `share_comment` WHERE id = ? limit 1;',
+        delete_article: 'DELETE FROM `share` WHERE id = ? limit 1;'
     },
     publish: {
         article: 'INSERT INTO `share` VALUES (0, ?, ?, ?, ?, 0, ?, ?);',
