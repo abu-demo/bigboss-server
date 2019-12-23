bd1237cce251660e77809b007b1540516addadf6
abu <abusuper@outlook.com>
2019-12-23 16:01:52 +0800

    delete
    delete article
    can't find article 404

--- routes/detail.js
+++ routes/detail.js
@@ -8,7 +8,13 @@ router.prefix('/detail')
 router.get('/', async (ctx, next) => {
     let param = ctx.request.query.id
     await query($sql.detail.get_artical, param).then(res => {
-        ctx.body = res
+        if (res.length == 0) {
+            // 账号密码不匹配
+            ctx.status = 404;
+            ctx.body = '文章不存在！';
+        } else {
+            ctx.body = res
+        }
     })
 })
 
@@ -24,6 +30,22 @@ router.get('/comment', async (ctx, next) => {
 router.post('/insert_comment', async (ctx, next) => {
     let params = ctx.request.body;
     await query($sql.detail.insert_comment, params).then(res => {
+        ctx.body = res
+    })
+})
+
+// 删评论
+router.get('/delete_comment', async (ctx, next) => {
+    let params = ctx.request.query.id;
+    await query($sql.detail.delete_comment, params).then(res => {
+        ctx.body = res
+    })
+})
+
+// 删文章
+router.get('/delete_article', async (ctx, next) => {
+    let params = ctx.request.query.id;
+    await query($sql.detail.delete_article, params).then(res => {
         ctx.body = res
     })
 })
--- utils/sql_map.js
+++ utils/sql_map.js
@@ -1,20 +1,22 @@
 let sql_map = {
     user: {
-        personal: 'SELECT  id, name, role, sign, logo, education, contact, web FROM user where id = ?;',
+        personal: 'SELECT  id, name, role, sign, logo, education, contact, web FROM `user` where id = ?;',
         register: 'INSERT INTO `user` VALUES(0, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
-        login: 'SELECT  id, name, password, role, sign, logo, education, contact, web FROM user where id = ?;'
+        login: 'SELECT  id, name, password, role, sign, logo, education, contact, web FROM `user` where id = ?;'
     },
     share: {
-        get: 'SELECT id, title, summary, author, author_id, time, discuss FROM share order by id desc;',
+        get: 'SELECT id, title, summary, author, author_id, time, discuss FROM `share` order by id desc;',
     },
     feedback: {
-        get: 'SELECT id, name, time, text FROM feedback order by id desc;',
+        get: 'SELECT id, name, time, text FROM `feedback` order by id desc;',
         insert_comment: 'INSERT INTO `feedback` VALUES(0, ?, ?, ?);',
     },
     detail: {
-        get_artical: 'SELECT * FROM share WHERE ID = ?;',
-        get_comment: 'SELECT * FROM shareComment WHERE share_id = ? order by id desc;',
-        insert_comment: 'INSERT INTO `shareComment` VALUES(0, ?, ?, ?, ?, ?);',
+        get_artical: 'SELECT * FROM `share` WHERE ID = ?;',
+        get_comment: 'SELECT * FROM `share_comment` WHERE share_id = ? order by id desc;',
+        insert_comment: 'INSERT INTO `share_comment` VALUES(0, ?, ?, ?, ?, ?);',
+        delete_comment: 'DELETE FROM `share_comment` WHERE id = ?;',
+        delete_article: 'DELETE FROM `share` WHERE id = ?;'
     },
     publish: {
         article: 'INSERT INTO `share` VALUES (0, ?, ?, ?, ?, 0, ?, ?);',
